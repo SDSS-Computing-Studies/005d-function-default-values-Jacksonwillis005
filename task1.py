a84a1328e03395d489d055fba0e033ac85e61a71
WillisJackson <jacksonw005@deltalearns.ca>
2021-03-04 09:13:16 -0800

    Update

--- task1.py
+++ task1.py
@@ -4,9 +4,11 @@
 Create a function called sentence() that takes 3 input paremters:
 greeting: string that will contain a salutation, examples: Hello, Hiya, Ola
 name: a string that will contain a person's name.  This should have a default value of "Benjamin"
-question: a string that will contain a full question to ask the person.  It should have a default value of "How are you"
+question: a string that will contain a full question to ask the person.  It should 
+have a default value of "How are you"
 
-Have the greeting, name and question combine together into a string. Note: You are not using a print statement, so you must
+Have the greeting, name and question combine together into a string. Note: 
+You are not using a print statement, so you must
 use the concatenate (+) operator. Make sure your spacing is appropriate.
 
 return value:
@@ -14,8 +16,12 @@ string. The output sentence
 
 example assertion:
 assert sentence("Hello") == "Hello Benjamin. How are you"
-assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. Have you enjoyed your meal"
+assert sentence("Hiya","Casey","Have you enjoyed your meal") == "Hiya Casey. 
+Have you enjoyed your meal"
 
 (2 points) 
 """
 
+def sentence(a,b="Benjamin",c="How are you"):
+
+    return print (sentence(a + b + c))
